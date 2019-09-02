ec412f49543e5c441f7ead28a609f1778714503d
Murilo Dutra <murilo@Mac-mini-de-ScorePointer.local>
2019-09-02 10:42:49 -0300

    Isolando os métodos de conexão

--- aluraframe/client/js/app/services/ConnectionFactory.js
+++ aluraframe/client/js/app/services/ConnectionFactory.js
@@ -1,30 +1,70 @@
-var stores = ['negociacoes'];
-var version = 4;
-var dbName = 'aluraframe';
+
+//Criando uma função anônima. Isso foi feito para envolver a classe ConnectionFactory envolto de um módulo
+
+var ConnectionFactory = (
+    function (){
+
+        const stores = ['negociacoes'];
+        const version = 4;
+        const dbName = 'aluraframe';
+        
+        var connection = null;
+        var close = null;
 
 
-class ConnectionFactory{
+        return class ConnectionFactory{
 
-    constructor(){
-        throw new Error('Não é possível criar instâncias de ConnectionFactory.');
-    }
+            constructor(){
+                throw new Error('Não é possível criar instâncias de ConnectionFactory.');
+            }
 
-    static getConnection(){
-        return new Promise((resolve, reject) => {
 
-            let openRequest = window.indexedDB.open(dbName, version); //Requisitando abertura de conexão
+            static getConnection(){
+                return new Promise((resolve, reject) => {
 
-            openRequest.onupgradeneeded = event => {
+                    let openRequest = window.indexedDB.open(dbName, version); //Requisitando abertura de conexão
 
-            };
+                    openRequest.onupgradeneeded = event => {
+                        ConnectionFactory._createStores(event.target.result); //Cria as Stores
+                    };
 
-            openRequest.onsuccess = event => {
+                    openRequest.onsuccess = event => {
+                        if(!connection){ //Para não devolver conexões diferentes
+                            connection = event.target.result;
+                            close = connection.close.bind(connection); //Obtendo o método original com seu THIS original
+                            connection.close = function(){ //sobrescrevendo o método close()
+                                throw new Error('Não é permitido fechar diretamente a conexão.');
+                            }
+                        }
+                        
+                        resolve(connection); //Caso tudo dê certo
+                    };
 
-            };
+                    openRequest.onerror = event => {
+                        console.log(event.target.error);
+                        reject(event.target.error.name); //Caso um erro aconteça
+                    };
+                });
+            }
 
-            openRequest.onerror = event => {
 
-            };
-        });
-    }
-}
+            static _createStores(connection){
+                stores.forEach(store => {
+
+                    if(connection.objectStoreNames.contains(store))    //Verificando se a ObjectStore já existe
+                        connection.deleteObjectStore(store);   //Deletando a ObjectStore já existente. Para atualizar uma ObjectStore, é necessário deletá-la e criá-la novamente
+
+                    connection.createObjectStore(store, {autoIncrement: true}); //Criando a ObjectStore    
+                });
+            }
+
+
+            static closeConnection(){
+                if(connection){
+                    close();
+                    //Reflect.apply(close, connection, []); Outra maneira de preservar o THIS de 'close'
+                    connection = null;
+                }
+            }
+        }
+}) ();  //Invocano a função anônima
